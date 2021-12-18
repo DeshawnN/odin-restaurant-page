0934033852d27321c1c07bc8e744875749327da6
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2021-12-18 08:08:05 -0500

    add: add menu scripts and json data

---
+++ src/menu.js
@@ -0,0 +1,31 @@
+import menu from './menu.json';
+import { clearContent, generateNavBar, generateMainContent } from './dom-manip';
+
+export function generateContent(element) {
+    const mainBody = element.querySelector('.main-body');
+    const menuDiv = document.createElement('div');
+    menuDiv.classList.add('menu');
+
+    for (const item of menu) {
+        const itemDiv = document.createElement('div');
+        itemDiv.classList.add('menu-item');
+        
+        const name = document.createElement('div');
+        name.textContent = item.name;
+        itemDiv.appendChild(name);
+
+        const price = document.createElement('div');
+        price.textContent = item.price.toFixed(2);
+        itemDiv.appendChild(price);
+        
+        menuDiv.appendChild(itemDiv);
+    }
+    mainBody.appendChild(menuDiv);
+}
+
+export function generateView(element) {
+    clearContent(element);
+    generateNavBar(element);
+    generateMainContent(element);
+    generateContent(element);
+}
